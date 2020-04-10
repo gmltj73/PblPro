47d179068a77d702b74dd5f05221b31e8db61f12
CHOISUNGEUN <cseswu17@naver.com>
2020-04-10 19:57:31 +0900

    200410 - 성은_권한추가

--- app/src/main/java/com/example/pblpro/MainActivity.java
+++ app/src/main/java/com/example/pblpro/MainActivity.java
@@ -1,59 +1,133 @@
 package com.example.pblpro;
 
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 
 
-public class MainActivity extends AppCompatActivity {
 
+
+public class MainActivity extends AppCompatActivity {
+    //git success
+    String g;
     static Uri uri;  //경로 변수
     Integer sw = null;  //스위치변수로 upload-0 download=1
+    int STORAGE_PERMISSION_CODE = 1;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Button uploadbt = (Button) findViewById(R.id.uploadbt); //업로드 버튼 객체 생성
         Button downloadbt = (Button) findViewById(R.id.downloadbt); //다운로드 버튼 객체 생성
+
+        //권한 요청 설정
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(MainActivity.this, "You have already granted this permission!",
+                    Toast.LENGTH_SHORT).show();
+        } else {
+            requestStoragePermission();
+        }
+        //여기까지
+
         uploadbt.setOnClickListener(new Button.OnClickListener() {
             public void onClick(View view) {
+
+
+
+
+
                 Intent intent = new Intent(Intent.ACTION_GET_CONTENT); //Intent 생성 //이미지 요청
                 intent.setType("*/*"); //파일포맷을 파일 전체로 정함
                 intent = Intent.createChooser(intent, "choose a file");
                 startActivityForResult(intent, 1000); // 이미지 요청 받음
                 sw = 0;
+
             }
 
         });
 
+
         downloadbt.setOnClickListener(new Button.OnClickListener() {
             public void onClick(View view) {  //**바뀐부분**
+
+
                 Intent intent = new Intent(Intent.ACTION_GET_CONTENT); //Intent 생성 //이미지 요청
                 intent.setType("*/*"); //파일포맷을 파일 전체로 정함
                 intent = Intent.createChooser(intent, "choose a file");
                 startActivityForResult(intent, 1000); // 이미지 요청 받음
                 sw = 1;
+
             }
 
         });
 
+
     }
 
+    private void requestStoragePermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)) {
+
+            new AlertDialog.Builder(this)
+                    .setTitle("Permission needed")
+                    .setMessage("This permission is needed because of this and that")
+                    .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions(MainActivity.this,
+                                    new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+                        }
+                    })
+                    .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    })
+                    .create().show();
+
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == STORAGE_PERMISSION_CODE)  {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission GRANTED", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Permission DENIED", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {  //startActivity 코드가 넘어옴
@@ -70,7 +144,7 @@ public class MainActivity extends AppCompatActivity {
             Context context = getApplicationContext();
 
             filePath = RealPathUtil.getRealPathFromURI_API19(context, uri);
-            System.out.println("============" + filePath);
+            System.out.println("==================" + filePath);
 
             try {
                 FileInputStream fis = null;
@@ -95,9 +169,9 @@ public class MainActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
 
+
         }
         System.out.println("ok");
-
 
         //**변한부분**
         if (sw == 0) {
